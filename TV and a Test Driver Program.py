3cb8f8b0be33aac67fc8222b65f356eae30f8587
Alexza Jean Catanoy <alexzacatanoy@gmail.com>
2023-05-14 20:26:17 +0800

    Course and Section

--- TV and a Test Driver Program.py
+++ TV and a Test Driver Program.py
@@ -8,4 +8,5 @@ print("\033[1;97mClass named TV and a Test Driver program named TestTV".center(9
 print("\033[0;36m*" * 90)
 
 print("\033[1;32m\nHello! Your programmers name is Alexza Jean.")
+print("\033[1;32m\nShe's from BSCPE 1-4")
 
