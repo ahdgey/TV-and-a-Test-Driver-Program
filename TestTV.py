542330f23c996017254494d0cc9074f8c30c3c9a
Alexza Jean Catanoy <alexzacatanoy@gmail.com>
2023-05-17 02:45:30 +0800

    Object 2 of the TV class

--- TestTV.py
+++ TestTV.py
@@ -14,8 +14,10 @@ class TestTV:
         #Turn on TV 1
         tv1.turn_on()
 
-        #Set TV 1's channel and volume level
+        #Set TV 1's channel 
         tv1.set_channel(30)
+
+        #Set TV 1's volume level
         tv1.set_volume(3)
 
         #Print the channel and volume level that is set for TV 1
@@ -23,3 +25,22 @@ class TestTV:
 
 
         #tv2's channel is 3 and volume level is 2
+
+        #Object 2 of the TV class
+        tv2 = TV()
+
+        #Turn on TV 2
+        tv2.turn_on()
+
+        #Set TV 2's channel
+        tv2.set_channel(3)
+
+        #Set TV 2's volume level
+        tv2.volume_level(2)
+
+        #Print the chennel and volume level that is set for TV 2
+        print("tv2's channel is",tv2.get_channel(), "and volume level is", tv2.get_volume_level())
+
+    if __name__== "__main__":
+        main()
+
