8a21c5e20ecc68bda579104224ef172c28bb1f81
Alexza Jean Catanoy <alexzacatanoy@gmail.com>
2023-05-17 02:08:54 +0800

    Two objects of the TV class

--- TestTV.py
+++ TestTV.py
@@ -3,4 +3,8 @@ class TestTV:
     #Import file TV Program.py
     from TV import TV
 
-    
+    #Methods of TestTV
+    def main():
+
+    #tv1's channel is 30 and volume level is 3
+    #tv2's channel is 3 and volume level is 2
